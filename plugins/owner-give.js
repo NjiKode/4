f511c0c20b5a2397c4adbe92fe863f8413cc96a2
PencariKode <pencaricode@gmail.com>
2022-05-27 16:31:52 +0700

    Add "Give" Feature

---
+++ plugins/owner-give.js
@@ -0,0 +1,30 @@
+const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
+    clearTimeout(this)
+    resolve()
+}, ms))
+
+let handler = async (m, {conn, args}) => {
+  console.log(args)
+  m.reply(args)
+  let arg
+  if (args[0] === "me") arg = m.sender
+  else arg = args[0]
+  let arg2 = args[1]
+  let arg3 = args[2]
+  
+  let user = global.db.data.users[arg]
+  let last = user[arg2]
+  delay(500);
+  user[arg2] = arg3 
+  
+  let newUser = global.db.data.users[arg][arg2]
+  
+  m.reply(`From: ${last}\nTo: ${newUser}`)
+  
+};
+
+handler.help = ["give"]
+handler.command = /^give$/i
+handler.tags = ["advanced"]
+
+export default handler
