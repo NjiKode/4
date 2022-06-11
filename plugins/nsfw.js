cdea4c8d858ca7940236593168af84d7d76cab70
PencariKode <pencaricode@gmail.com>
2022-06-11 12:55:12 +0700

    Test Fix NSFW

--- plugins/nsfw.js
+++ plugins/nsfw.js
@@ -1,10 +1,10 @@
-import fetch from "node-fetch"
+import fetch from 'node-fetch'
 
 
 let handler = async (m, {conn, command}) => {
   
-  if (command === "nsfwglass") command = "glasses"
-  if (command === "nsfwfoot") command = "foot"
+  if (command === 'nsfwglass') command = 'glasses'
+  if (command === 'nsfwfoot') command = 'foot'
   let api = `https://api-yogipw.herokuapp.com/api/nsfw/${command}`
   try {
     let result = await fetch(api).then(a => a.json()).then(b => b.result)
@@ -16,8 +16,8 @@ let handler = async (m, {conn, command}) => {
   }
 }
 
-handler.help = ["ass", "ahegao", "bdsm", "blowjob", "cuckold", "cum", "ero", "nsfwfoot", "nsfwglass", "hentai", "hentaigif", "jahy", "masturbate", "nsfwneko", "orgy", "panties", "pussy", "yuri", "thighs"]
-handler.tags = ["nsfw"]
+handler.help = ['ass', 'ahegao', 'bdsm', 'blowjob', 'cuckold', 'cum', 'ero', 'nsfwfoot', 'nsfwglass', 'hentai', 'hentaigif', 'jahy', 'masturbate', 'nsfwneko', 'orgy', 'panties', 'pussy', 'yuri', 'thighs']
+handler.tags = ['nsfw']
 handler.command = /^ass|ahegao|bdsm|blowjob|cuckold|cum|ero|nsfw(foot|glass|neko)|hentai(gif)?|jahy|masturbate|orgy|panties|pussy|yuri|thighs$/i
 handler.nsfw = true
 handler.limit = true
